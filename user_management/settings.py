adcd53f485e0b12f54a0e8ade1e93ee4b484c8a0
Smitrajani281203 <117822207+Smitrajani281203@users.noreply.github.com>
2023-10-28 23:08:02 +0530

    base and home html pages update
    base and home page modification

--- user_management/settings.py
+++ user_management/settings.py
@@ -168,3 +168,8 @@ SESSION_COOKIE_AGE = 60 * 60 * 24 * 30
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+STATICFILES_DIRS = [
+    BASE_DIR,"static"
+]
