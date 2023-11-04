8cdcfa1b624ffed395b8e42f4ce72d40b4df98f0
Devanshvora03 <131452370+Devanshvora03@users.noreply.github.com>
2023-11-04 16:46:09 +0530

    Map URLS Added

--- users/models.py
+++ users/models.py
@@ -6,7 +6,6 @@ from PIL import Image
 # Extending User Model Using a One-To-One Link
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(default='default.jpg', upload_to='profile_images', blank=True)
     bio = models.TextField()
     phone = models.CharField(max_length = 12, blank=True, null=True)
     address = models.CharField(max_length = 100, blank=True, null=True)
@@ -25,6 +24,7 @@ class Expense(models.Model):
         return self.user_id.username + "'s expense at " + str(self.date)
     
 class Coordinate(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     date_time = models.DateTimeField(blank=True, null=True, auto_now_add=True)
