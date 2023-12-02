28eabf11d9a62643a4e2db53fc850cce43a2a365
Devanshvora03 <131452370+Devanshvora03@users.noreply.github.com>
2023-12-02 19:14:32 +0530

    Middleware Started

--- user_management/settings.py
+++ user_management/settings.py
@@ -54,7 +54,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-
+    'users.middleware.access_middleware',
 ]
 
 ROOT_URLCONF = 'user_management.urls'
