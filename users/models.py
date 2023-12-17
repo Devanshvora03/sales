2f56f281804e0663811f136b107843c274c184b9
Devanshvora03 <131452370+Devanshvora03@users.noreply.github.com>
2023-12-17 11:43:34 +0530

    Person Hospital Model Added

--- users/models.py
+++ users/models.py
@@ -11,6 +11,7 @@ class Profile(models.Model):
     def __str__(self):
         return self.user.username
 
+
 class Expense(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE)
     amount = models.CharField(max_length=10)
@@ -26,21 +27,43 @@ class Expense(models.Model):
     def __str__(self):
         return self.user_id.username + "'s expense at " + str(self.date)
     
+
+class Person(models.Model):
+    name = models.CharField(max_length=25, null=True)
+    dept = models.CharField(max_length=20, null=True)
+
+    def __str__(self):
+            return self.name
+
+
+class Hospital(models.Model):
+    hospital_name = models.CharField(max_length=50, null=True)
+    hospital_address = models.CharField(max_length=150, null=True)
+    
+    def __str__(self):
+        return self.name
+    
+
 class Coordinate(models.Model):
     user_id = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    person = models.ForeignKey(Person, on_delete=models.CASCADE, blank=True, null=True)
     latitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     longitude = models.DecimalField(max_digits=9, decimal_places=6, blank=True, null=True)
     date_time = models.DateTimeField(blank=True, null=True, auto_now_add=True)
+    product  = models.CharField(max_length=50, null=True)
+    outcome = models.CharField(max_length=50, null=True)
 
     def __str__(self):
         return 'Coordinates of' + str(self.user_id) + 'at' + str(self.date_time)
-    
+
+
 class ManagerProfile(models.Model):
     user_id = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True)
     company_name = models.CharField(max_length=20, blank=True, null=True)
 
     def __str__(self):
         return self.user_id.username
+
 
 class EmployeeManager(models.Model):
     employee = models.OneToOneField(User, on_delete=models.CASCADE, blank=True, null=True, verbose_name='Employee', related_name='employee')
