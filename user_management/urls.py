8a225330c1d722dfa9c4aff866f5ae25d3b0a4da
Smitrajani28 <smitrajani28@gmail.com>
2023-12-22 20:20:22 +0530

    small changes in urls.py in user and home.html and css

--- user_management/urls.py
+++ user_management/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('', include('users.urls')),
     path('login/', CustomLoginView.as_view(redirect_authenticated_user=True, template_name='users/login.html',
                                            authentication_form=LoginForm), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='logout'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='users/home.html'), name='logout'),
     path('password-reset/', ResetPasswordView.as_view(), name='password_reset'),
     path('password-reset-confirm/<uidb64>/<token>/',
          auth_views.PasswordResetConfirmView.as_view(template_name='users/password_reset_confirm.html'),
